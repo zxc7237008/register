16777d2cf6171fe362e97dd4296bb30212f89130
qh95918 <qh95918@foxmail.com>
2019-05-07 17:47:37 +0800

    han

---
+++ register/src/main/java/register/T.java
@@ -0,0 +1,15 @@
+package register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class T {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(T.class, args);
+	}
+}
